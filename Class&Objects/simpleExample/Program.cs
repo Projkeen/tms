277a460c61aad297e6df3fe002e33e153bd195cd
Projkeen <103644317+Projkeen@users.noreply.github.com>
2022-04-21 14:15:20 +0300

    Add files via upload

---
+++ Class&Objects/simpleExample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Product
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product Fish = new Product();
+            Fish.title = "salmon";
+            Fish.weight = 1;
+            Fish.GetInfo();
+
+            Product Chips = new Product();
+            Chips.title = "Pringles";
+            Chips.taste = "Cheese";
+            Chips.GetInfo();
+
+            Product Whiskey = new Product();
+            Whiskey.title = "Ballantine's";
+            Whiskey.volume = 0.7;
+            Whiskey.GetInfo();
+
+            Console.ReadKey();
+
+        }
+    }
+    public class Product
+    {
+        public string title;
+        public int weight;
+        public string taste ;
+        public double volume;
+        public void GetInfo()
+        {
+            Console.WriteLine($"продукт: {title}, вес: {weight} кг, вкус {taste}, объем {volume} ");
+        }
+    }
+}
